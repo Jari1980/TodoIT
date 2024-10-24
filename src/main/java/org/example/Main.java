5cadc20d067076d2f2b13d3f37b5bca21770a3c4
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-24 18:37:00 +0200

    person class with non nullable fields created

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,13 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        //Testing - Seems to work, cant set fields to null without error and message
+        Person test = new Person("Jari", "Testar", "epost");
+        test.getSummary();
+        Person test2 = new Person("Test", "igen då", "mm");
+        test2.setEmail("kk");
+        test2.getSummary();
+
+
     }
 }
