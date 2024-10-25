a12bdc079e94216fdb9a538d2ca58fbfd512fa67
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-25 09:58:17 +0200

    TodoItemTask class done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -24,7 +24,16 @@ public class Main {
 
         System.out.println("---------------------");
         TodoItem todoTest = new TodoItem("Todo", "Do this and that", LocalDate.now().plusDays(1), false, test);
+        TodoItem todoTest2 = new TodoItem("Todo2", "More stuff to do", LocalDate.now().plusDays(2), false, test);
         todoTest.getSummary();
         System.out.println("Is overtime: " + todoTest.isOverdue());
+
+        System.out.println("--------------------");
+        TodoItemTask task1 = new TodoItemTask(todoTest, test);
+        TodoItemTask task2 = new TodoItemTask(todoTest2);
+        task1.getSummary();
+        task2.getSummary(); //assigned false with no assignee
+        task2.setAssignee(test);
+        task2.getSummary(); //assigned true with assignee
     }
 }
