065b0ca3db2743139fe3c9a2457d9b49b81fef9f
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-11-11 14:45:05 +0100

    sequencers created

---
+++ src/main/java/org/example/sequencers/PersonIdSequencer.java
@@ -0,0 +1,17 @@
+package org.example.sequencers;
+
+public class PersonIdSequencer {
+    private static int currentId = 0;
+
+    public static int nextId(){
+        currentId += 1;
+        return currentId;
+    }
+    public static int getCurrentId(){
+        return currentId;
+    }
+    public static void setCurrentId(int id){
+        currentId = id;
+    }
+
+}
